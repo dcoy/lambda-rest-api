b8e79042a1f98eaee7728d6df0e926877d2a5440
David Coy <me@david-coy.com>
2018-06-06 17:27:23 -0500

    Skeleton REST endpoints

---
+++ handler.js
@@ -0,0 +1,91 @@
+'use strict';
+
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Note.create(JSON.parse(event.body))
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Unable to create the note.'
+        }));
+    });
+};
+
+module.exports.getOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Note.find()
+        .then(notes => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(notes)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Unable to fetch your note.'
+        }))
+    });
+};
+
+module.exports.getAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Note.find()
+        .then(notes => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(notes)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Unable to fetch the notes.'
+        }));
+    });
+};
+
+module.exports.update = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Node.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Unable to fetch the notes.'
+        }));
+    });
+};
+
+module.exports.delete = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Node.findByIdAndRemove(event.pathParameters.id)
+        .then(node => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Removed note with id: ' + note._id, node: note })
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Unable to fetch the notes.'
+        }));
+    });
+};
